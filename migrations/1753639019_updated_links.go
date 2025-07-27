41eff0ac1c7249b803edcfe1e6aca49ce6f21c29
Jan <jan-d.pietsch@live.de>
2025-07-27 19:57:42 +0200

    add comment field to links-table

---
+++ migrations/1753639019_updated_links.go
@@ -0,0 +1,45 @@
+package migrations
+
+import (
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_449060851")
+		if err != nil {
+			return err
+		}
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(5, []byte(`{
+			"autogeneratePattern": "",
+			"hidden": false,
+			"id": "text2490651244",
+			"max": 0,
+			"min": 0,
+			"name": "comment",
+			"pattern": "",
+			"presentable": false,
+			"primaryKey": false,
+			"required": false,
+			"system": false,
+			"type": "text"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_449060851")
+		if err != nil {
+			return err
+		}
+
+		// remove field
+		collection.Fields.RemoveById("text2490651244")
+
+		return app.Save(collection)
+	})
+}
